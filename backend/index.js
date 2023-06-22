b108028fcd9197a8cdcbd1c55aa738b0521ed1fb
raobaba <raorajan9576@gmail.com>
2023-06-22 13:25:45 +0530

    database is connected

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 require('dotenv').config(); 
 const cors = require("cors"); 
 const app = express();
+const {Connection} = require("./Database/db.js");
 app.use(express.json()); 
 app.use(cors());
+Connection();
 app.listen(process.env.PORT, () =>
  console.log(`Server is running successfully on PORT ${process.env.PORT}`)
  ); 
