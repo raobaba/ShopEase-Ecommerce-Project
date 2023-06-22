6e9a05581c24c0248a120e9497a33b3c6ec784b2
raobaba <raorajan9576@gmail.com>
2023-06-22 13:19:33 +0530

    server is running

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-console.log('going to create backend server');
+const express = require("express");
+require('dotenv').config(); 
+const cors = require("cors"); 
+const app = express();
+app.use(express.json()); 
+app.use(cors());
+app.listen(process.env.PORT, () =>
+ console.log(`Server is running successfully on PORT ${process.env.PORT}`)
+ ); 
