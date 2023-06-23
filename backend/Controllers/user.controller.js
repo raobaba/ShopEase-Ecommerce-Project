9f96cf078070f0548001f8fb6959f2cb63c889a6
raobaba <raorajan9576@gmail.com>
2023-06-23 09:25:41 +0530

    user get update delete working

--- backend/Controllers/user.controller.js
+++ backend/Controllers/user.controller.js
@@ -21,7 +21,80 @@ const registerUser = async (req, res) => {
     }
 };
 
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await UserModel.findOne({ email });
+
+        if (user) {
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (result) {
+                    const token = jwt.sign({ userID: user._id }, process.env.key);
+                    res.send({ "msg": "Login Successful", "token": token });
+                } else {
+                    res.send("Wrong Credentials");
+                }
+            });
+        } else {
+            res.send("Wrong Credentials");
+        }
+    } catch (err) {
+        res.send("Something went wrong");
+        console.log(err);
+    }
+};
+
+const getUser = async (req, res) => {
+    try {
+        const users = await UserModel.find();
+        res.json(users);
+      } catch (err) {
+        res.status(500).json({ error: "Failed to fetch users" });
+      }
+  };
+  
+  const updateUser = async (req, res) => {
+    const { fullName, userName, email, password } = req.body;
+  
+    try {
+      const user = await UserModel.findById(req.params.userId);
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+  
+      user.fullName = fullName;
+      user.userName = userName;
+      user.email = email;
+  
+      if (password) {
+        const hashedPassword = await bcrypt.hash(password, 5);
+        user.password = hashedPassword;
+      }
+  
+      await user.save();
+      res.json({ message: "User updated successfully" });
+    } catch (err) {
+      res.status(500).json({ error: "Failed to update user" });
+    }
+  };
+  
+  const deleteUser = async (req, res) => {
+    try {
+      const user = await UserModel.findByIdAndDelete(req.params.userId);
+      if (!user) {
+        return res.status(404).json({ error: "User not found" });
+      }
+      res.json({ message: "User deleted successfully" });
+    } catch (err) {
+      res.status(500).json({ error: "Failed to delete user" });
+    }
+  };
   
   module.exports = {
-    registerUser
+    registerUser,
+    loginUser,
+    getUser,
+    updateUser,
+    deleteUser,
   };
--- backend/Routes/user.route.js
+++ backend/Routes/user.route.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const { authenticate } = require("../Middlewares/authenticate.middleware");
 const {
-  registerUser
+  registerUser,
+  loginUser,
+  getUser,
+  updateUser,
+  deleteUser,
 } = require("../Controllers/user.controller.js");
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
-
+userRouter.post("/login", loginUser);
+userRouter.get("/getUser", authenticate, getUser);
+userRouter.put("/:userId", authenticate, updateUser);
+userRouter.delete("/:userId", authenticate, deleteUser);
 
 module.exports = userRouter;
