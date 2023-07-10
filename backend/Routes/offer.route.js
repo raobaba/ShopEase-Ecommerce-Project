0c75db9e6db820eed7ec5ac33dd03ed9d1da98d5
raobaba <raorajan9576@gmail.com>
2023-07-10 09:12:26 +0530

    offer route creted

---
+++ backend/Routes/offer.route.js
@@ -0,0 +1,11 @@
+const offerController = require('./offerController'); 
+const express = require('express');
+const offerRouter = express.Router();
+
+offerRouter.get('/offers', offerController.getAllOffers);
+offerRouter.get('/offers/:id', offerController.getOfferById);
+offerRouter.post('/offers', offerController.createOffer);
+offerRouter.put('/offers/:id', offerController.updateOffer);
+offerRouter.delete('/offers/:id', offerController.deleteOffer);
+
+module.exports = offerRouter;
